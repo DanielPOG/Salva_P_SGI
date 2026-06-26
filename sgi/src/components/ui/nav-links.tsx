94922b5aee2d8cb02717578d85d62abc2f3d0f68
Daniel Santiago Caicedo Rodriguez <daniels_caicedo@soy.sena.edu.co>
2026-06-26 08:31:33 -0500

    feature: Aside UI

---
+++ sgi/src/components/ui/nav-links.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import Link from 'next/link';
+import {
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
+  PhoneIcon,
+} from '@heroicons/react/24/outline';
+
+import { usePathname } from 'next/dist/client/components/navigation';
+const links = [
+  { name: 'Home', href: '/', icon: HomeIcon },
+  {
+    name: 'Lugares',
+    href: '/locations',
+    icon: DocumentDuplicateIcon,
+  },
+  { name: 'Compañias', href: '/companies', icon: UserGroupIcon },
+  { name: 'Dispositivos', href: '/devices', icon: PhoneIcon },
+];
+
+export default function NavLinks() {
+  const pathname = usePathname();
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className=
+            {   ` flex h-12 grow items-center justify-center gap-2 rounded-md 
+                bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none 
+                md:justify-start md:p-2 md:px-3  border-b-2 border-pink-300
+                ${pathname === link.href ? 'bg-sky-300': ''}
+                `}
+          
+          >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+            
+          </Link>
+        );   
+      })}
+    </>
+  );
+}
---
+++ sgi/src/components/ui/sidenav.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import NavLinks from '@/components/ui/nav-links'
+export default function SideNav(){
+    return(
+        <div className="flex h-full flex-col px-3 py-4 md:px-2">
+            <Link 
+                className="mb-2 flex h-20 items-center justify-center rounded-md bg-sky-300  md:h-40 "
+                href="/">
+                <div className="md:w-full"> 
+                    <Image 
+                    src="/logo_salva.webp" 
+                    width={200} height={50} className="w-auto h-auto" loading="eager" alt="Logo Salva" />
+                </div>
+            </Link>
+            <div className="flex  grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2 ">
+                <NavLinks />
+                <div className="hidden h-auto w-full grow rounded-md bg-gray-700 md:block"></div>
+            </div>
+        </div>
+    )
+}
